2851203dc8ffc0584b073d060e5e4e6a8e15e569
ANDRES <andres2661991@gmail.com>
2021-03-07 17:27:01 -0300

    PORT

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(4545, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 4545'); // eslint-disable-line no-console
   });
 });
